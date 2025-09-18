718111f27e5a71f1a3b8512a0aedc741e80aa2c3
Em Karisch <em@khaos.solutions>
2025-09-18 09:08:57 +0200

    feat: unlock heat sensor alongside heat pipes (#2)

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,5 @@
 -- Better subgroup placement with SchallCircuitGroup
 require("data-updates.schall-circuit-group")
+
+-- Unlock heat sensor together with heat pipes
+require("data-updates.unlock-tech")
